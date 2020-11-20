545a2701d8fa13d49f3f180d1401bcbf1a8d93aa
honeylottu <honey.lottu@gmail.com>
2020-11-20 22:02:13 +0300

    First LB

---
+++ grigoreva1/grigoreva/src/main/java/GrigorevaIST813/grigoreva/GrigorevaApplication.java
@@ -0,0 +1,13 @@
+package GrigorevaIST813.grigoreva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrigorevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrigorevaApplication.class, args);
+	}
+
+}
